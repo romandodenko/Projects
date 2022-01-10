10c48c982d621cd7a885b86e0bb3a43b6ced68e0
Roman Dodenko <creed6695@mail.ru>
2022-01-10 23:07:06 +0700

    added

---
+++ High-pass/src/js/main.js
@@ -0,0 +1,32 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+
+import * as burger from './modules/burger.js'
+
+burger.burgerOpen()
+
+import * as aboutForm from './modules/about.js'
+
+aboutForm.aboutForm()
+
+import * as contactsMap from './modules/yandex-map.js'
+
+contactsMap.contactsMap()
+
+import * as contactsBtn from './modules/contacts.js'
+
+contactsBtn.contactsBtn()
+
+import * as contactsForm from './modules/contacts.js'
+
+contactsForm.contactsForm()
+
+import * as aosScr from './modules/aos.js'
+
+aosScr.aosScript()
+
+
+import * as gsapScr from './modules/gsap.js'
+
+gsapScr.gsapScript()
