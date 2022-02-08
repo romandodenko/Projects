8278786ae0b1ec2626b5069caf6c04a61fc8e6f2
Roman Dodenko <creed6695@mail.ru>
2022-02-08 16:00:01 +0700

    фикс хеадера

--- Sitdownpls/src/js/components/burger.js
+++ Sitdownpls/src/js/components/burger.js
@@ -1,9 +1,17 @@
-const burger = document.querySelector(".header__burger");
-const nav = document.querySelector(".nav-list-menu");
-const body = document.querySelector("body");
+const main = document.querySelector("main")
 
-burger.addEventListener("click", function() {
-  burger.classList.toggle("burger-active")
-  nav.classList.toggle("nav-active")
-  body.classList.toggle("body-block")
+document.querySelectorAll(".header__burger").forEach(function(burgerWrapper) {
+  burgerWrapper.addEventListener("click", function() {
+    burgerWrapper.classList.toggle("burger-active")
+    document.querySelector(".nav-list-menu").classList.toggle("nav-active")
+    document.querySelector("body").classList.toggle("body-block")
+  })
+})
+
+main.addEventListener("click", function() {
+  document.querySelectorAll(".header__burger").forEach(function(closeBur) {
+    closeBur.classList.remove("burger-active")
+  })
+  document.querySelector(".nav-list-menu").classList.remove("nav-active")
+  document.querySelector("body").classList.remove("body-block")
 })
