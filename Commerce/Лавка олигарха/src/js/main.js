0164c8afee3af713650db094c3be1449ee6b443b
Roman Dodenko <creed6695@mail.ru>
2022-08-30 01:40:44 +0700

    Новый проект - Лавка Олигарха

---
+++ Commerce/Лавка олигарха/src/js/main.js
@@ -0,0 +1,39 @@
+import * as overall from './modules/overall.js'
+
+overall.overall()
+
+import * as smoothScroll from './modules/smooth-scroll.js'
+
+smoothScroll.smoothScroll()
+
+import * as dynamic from './modules/dynamic-adaptive.js'
+
+dynamic.dynamic()
+
+import * as tabs from './modules/tabs.js'
+
+tabs.tabs()
+
+import * as show from './modules/show.js'
+
+show.show()
+
+import * as selectChoices from './modules/select-choices.js'
+
+selectChoices.selectChoices()
+
+import * as accordionTwo from './modules/accordion-2.js'
+
+accordionTwo.accordionTwo()
+
+import * as swiper from './modules/slider.js'
+
+swiper.swiper()
+
+import * as tooltip from './modules/tooltip.js'
+
+tooltip.tooltip()
+
+import * as inputTel from './modules/input-tel.js'
+
+inputTel.inputTel()
