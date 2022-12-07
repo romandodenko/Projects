f333213b385d885f99a9f1ef8cbd8b820183e27c
Roman Dodenko <creed6695@mail.ru>
2022-12-07 16:16:03 +0700

    delete oxem

--- Test/gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-  app.plugins.browsersync.init({
-    server: {
-      baseDir: `${app.path.dev.dist}`,
-    },
-    notify: false,
-    port: 3000,
-  });
-}
--- Test/src/js/modules/smooth-scroll.js
+++
@@ -1,8 +0,0 @@
-// Плавное перемещение по якорям
-
-var scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true
-});
-
-// Плавное перемещение по якорям
