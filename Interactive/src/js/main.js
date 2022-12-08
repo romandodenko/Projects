037424d9ed099f5242d63b9cce49894674dbbedf
Roman Dodenko <creed6695@mail.ru>
2022-12-08 22:33:08 +0700

    Create

---
+++ Interactive/src/js/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+window.onload = function () {
+
+  --include("./modules/document.js")
+
+  --include("./modules/range.js")
+
+    --include("./modules/slider.js")
+
+    --include("./modules/validate.js")
+
+    --include("./modules/smooth-scroll.js")
+
+    --include("./modules/input-tel.js")
+    
+}
